bae85cf94851fa4e186433da5ac74bf60ccfc6a3
Xenoxido <43368703+Xenoxido@users.noreply.github.com>
2019-03-20 21:34:44 +0100

    PuñoYPatadaVersion

--- Assets/Scripts/GameScene/CharMovement.cs
+++ Assets/Scripts/GameScene/CharMovement.cs
@@ -13,10 +13,12 @@ public class CharMovement : MonoBehaviour
     public float terminalVelocity = -10.0f;
     public float minFall = -1.5f;
     private float _vertSpeed;
+    private bool kick = false, hoop = false, jump = false;
     // Start is called before the first frame update
     void Start()
     {
-        _charController = GetComponent<CharacterController>();        _vertSpeed = minFall;
+        _charController = GetComponent<CharacterController>();
+        _vertSpeed = minFall;
         _animator = GetComponent<Animator>();
     }
 
@@ -25,6 +27,19 @@ public class CharMovement : MonoBehaviour
     {
         Vector3 movement = Vector3.zero;
         float horInput = Input.GetAxis("Horizontal");
+        if (Input.GetKeyDown(KeyCode.J) && !jump && !hoop) //Reconocimiento de la patada, la corutina lo para
+        {
+            _animator.SetBool("Kick", true);
+            kick = true;
+            StartCoroutine(offKick());
+        }
+        if (Input.GetKeyDown(KeyCode.K) && !jump && !kick) //Reconocimiento de la patada, la corutina lo para
+        {
+            _animator.SetBool("Hoop", true);
+            hoop = true;
+            StartCoroutine(offHoop());
+        }
+        if (kick || hoop) horInput = 0.0f;
 
         if (horInput != 0)
         {
@@ -35,11 +50,14 @@ public class CharMovement : MonoBehaviour
         }
 
         _animator.SetFloat("Speed", movement.sqrMagnitude);
+        
         if (_charController.isGrounded)
         {
+            jump = false;
             if (Input.GetButtonDown("Jump") || Input.GetKeyDown(KeyCode.W))
             {
                 _vertSpeed = jumpSpeed;
+                jump = true;
             }
             else
             {
@@ -59,6 +77,25 @@ public class CharMovement : MonoBehaviour
         movement.y = _vertSpeed;
 
         movement *= Time.deltaTime;
-        _charController.Move(movement);
+        _charController.Move(movement);
+        
+        
+        
+    }
+
+    private IEnumerator offKick()
+    {
+        yield return new WaitForSeconds(1);
+        _animator.SetBool("Kick", false);
+        yield return new WaitForSeconds(.5f);
+        kick = false;
+    }
+
+    private IEnumerator offHoop()
+    {
+        yield return new WaitForSeconds(1);
+        _animator.SetBool("Hoop", false);
+        yield return new WaitForSeconds(.5f);
+        hoop = false;
     }
 }
